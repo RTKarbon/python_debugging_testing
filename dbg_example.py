44980f554c797fde5a9ec9d0f534a79ac193648f
Alexey <lehalavr@ya.ru>
2018-11-13 23:01:42 -0500

    dbglib examples

---
+++ dbg_example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import dbglib as dbg
+
+dbg.print_info("This is info")
+dbg.print_success("This is success")
+dbg.print_warning("This is warning")
+dbg.print_error("This is an error")
